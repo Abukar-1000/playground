baf3a4f12b6e636af97ca74406c76edd76f473dc
Abukar-1000 <abukardjama1@gmail.com>
2022-09-07 23:22:34 -0400

    playground

---
+++ test/externalAlert.js
@@ -0,0 +1,12 @@
+/* TITLE: Course Page
+AUTHOR: Abukar Djama
+PURPOSE: Class Index Page to Host Lab Work for N341
+NOTE: Based on template provided by instructor
+ORIGINALLY CREATED ON: 08/24/2022
+LAST MODIFIED ON: 08/27/2022
+LAST MODIFIED BY: Abukar Djama
+MODIFICATION HISTORY: 
+08/24/2022: Created page & hosted on tesla server
+08/27/2022: added header gaurd onto file
+*/
+alert("Hows it going?");
