8fcfb7a17f30e80695f57077200cfea29fdbcd7d
Abukar-1000 <abukardjama1@gmail.com>
2022-12-14 13:05:50 -0500

    got mouse position

--- baseC/Program.cs
+++ baseC/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using System.Windows;
 using System.Net;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Json.Net;
-
+// using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
 namespace baseC
 {
     public class JsonResponse
@@ -12,15 +12,45 @@ namespace baseC
         public string response = "";
     }
 
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
     public class Program
     {
+        public static POINT point;
+        public static int xPos,yPos;
+
         public static void Main(string[] args) 
         {
             string strJson = Program.makeRequest("https://stormy-jumper.cyclic.app/F99");
             JsonResponse product = new JsonResponse();
             JsonResponse deserializedProduct = JsonConvert.DeserializeObject<JsonResponse>(strJson);
             // dynamic jsonResponse = new JObject();
+
             Console.WriteLine("Vitality: " + deserializedProduct.vitality + "\nResponse: " + deserializedProduct.response);
+            
+            while (true)
+            {
+                Program.GetPointPos();
+            }
+        }
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+        
+        public static void GetPointPos()
+        {
+            Program.GetCursorPos(out Program.point);
+            // Program.xPos = Program.point.X;
+            // Program.yPos = Program.point.Y;
+            Console.WriteLine($"({Program.point.X},{Program.point.Y})");
+            Thread.Sleep(500);
         }
         public static string makeRequest(string url)
         {
